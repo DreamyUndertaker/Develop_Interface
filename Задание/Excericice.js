82dfa0de9731e8aa9f5696b1ced4ff68b5d1e4b7
Vadim Zalogin <vadiimreg73@gmail.com>
2022-02-12 11:40:44 +0400

    5 лаба готова

--- Задание/Excericice.js
+++ Задание/Excericice.js
@@ -4,13 +4,10 @@ let m, n;
 m = Math.cos(5);
 n = Math.sin(666);
 
-//function ctg(x) {return 1 / Math.tan;}
 
-for (x = -3; x <= 3; x = x + 0.2)
-{
+for (x = -3; x <= 3; x = x + 0.2){
     y = (Math.tan(Math.pow(x, 6)) - Math.pow(-99,666, -x)) / (Math.cos(Math.tan(Math.pow(1 / (Math.pow((m / n) / n, 2))))) + Math.pow(69,666, m - n));
     console.log(Math.trunc(y));
-        
 }
 
 if(Number.isSafeInteger(y)){
